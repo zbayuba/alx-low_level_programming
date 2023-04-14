4b3c04c2f74ce156be3ecdfe520d767490566b06
zbayuba <ayoubouakrim82@gmail.com>
2023-04-13 17:58:20 -0700

    task n 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,8 +1,7 @@
 #include <stdlib.h>
 #include "main.h"
 /**
-
- *string_nconcat - concatenates n bytes of a string to another string
+ * string_nconcat - concatenates n bytes of a string to another string
  * @s1: string to append to
  * @s2: string to concatenate from
  * @n: number of bytes from s2 to concatenate to s1
